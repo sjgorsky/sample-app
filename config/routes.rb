2a96d782fdf116cd2132e493d90141998b6e4321
Scott Gorsky <scottgorsky@Scott-Gorskys-MacBook-Pro.local>
2022-01-20 18:12:02 -0600

    password reset initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get 'sessions/new'
   root 'static_pages#home'
 
@@ -11,5 +13,5 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
